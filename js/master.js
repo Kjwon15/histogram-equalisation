2985040edd8c13050a004ead220289acd4a4d7a8
Kang Jinwon <kjwonmail@gmail.com>
2015-03-26 00:51:19 +0000

    Support coloured image
    Convert rgb to yuv for easy transform.

--- js/master.js
+++ js/master.js
@@ -105,7 +105,11 @@ function getHistogram(imageData) {
     accumulated[i] = 0;
   }
   for (var i = 0; i < size; i += 1) {
-    histogram[imageData.data[i*4+1]] += 1;
+    var r = imageData.data[i*4+0];
+    var g = imageData.data[i*4+1];
+    var b = imageData.data[i*4+2];
+    var l = getLightness(r, g, b);
+    histogram[l] += 1;
   }
 
   for (var i = 0; i < 256; i += 1) {
@@ -123,18 +127,52 @@ function getHistogram(imageData) {
   };
 }
 
+function getLightness(r, g, b) {
+  return Math.round(r * 0.299 + g * 0.587 + b * 0.114);
+}
+
+function toYuv(r, g, b) {
+  var y = r * 0.299 + g * 0.587 + b * 0.114;
+  var u = 0.492 * (b - y);
+  var v = 0.877 * (r - y);
+
+  return {
+    y: y,
+    u: u,
+    v: v,
+  };
+}
+
+function toRgb(y, u, v) {
+  var r = y + v * 1.14;
+  var g = y - u * 0.395 - v * 0.518;
+  var b = y + u * 2.033;
+
+  return {
+    r: r,
+    g: g,
+    b: b,
+  }
+}
+
 function equalise(histogram, imageData) {
-  var accumulated = [];
-  var acc = 0;
   var newImageData = new Object(imageData);
   var size = newImageData.width * newImageData.height;
   var equalised = histogram.accumulated;
 
 
   for (var i = 0; i < size; i += 1) {
-    newImageData.data[i*4] = equalised[imageData.data[i*4]] * 255;
-    newImageData.data[i*4+1] = equalised[imageData.data[i*4+1]] * 255;
-    newImageData.data[i*4+2] = equalised[imageData.data[i*4+2]] * 255;
+    var r = newImageData.data[i*4+0];
+    var g = newImageData.data[i*4+1];
+    var b = newImageData.data[i*4+2];
+
+    var yuv = toYuv(r, g, b);
+    var newLightness = equalised[Math.round(yuv.y)] * 255;
+    var newRgb = toRgb(newLightness, yuv.u, yuv.v);
+
+    newImageData.data[i*4+0] = Math.round(newRgb.r);
+    newImageData.data[i*4+1] = Math.round(newRgb.g);
+    newImageData.data[i*4+2] = Math.round(newRgb.b);
   }
 
   return imageData;
